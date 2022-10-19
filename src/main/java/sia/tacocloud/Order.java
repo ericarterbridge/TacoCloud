f60545d1cd1474b67b9e47cce5b41842a2d40649
ericarterbridge <ericarterbridge@gmail.com>
2022-10-18 21:41:47 -0400

    created order class

---
+++ src/main/java/sia/tacocloud/Order.java
@@ -0,0 +1,10 @@
+package sia.tacocloud;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.CreditCardNumber;
+import javax.validation.constraints.NotBlank;
+import lombok.Data;
+
+public class Order {
+}
