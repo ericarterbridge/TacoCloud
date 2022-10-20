49d90563de71d5fb7d761bd526708f4592351408
ericarterbridge <ericarterbridge@gmail.com>
2022-10-19 21:52:22 -0400

    added imports to ordercontroller

--- src/main/java/sia/tacocloud/Controller/OrderController.java
+++ src/main/java/sia/tacocloud/Controller/OrderController.java
@@ -1,4 +1,15 @@
 package sia.tacocloud.Controller;
 
+import javax.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
 public class OrderController {
 }
