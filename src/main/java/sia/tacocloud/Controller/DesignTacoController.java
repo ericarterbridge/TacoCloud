1ed1b79dc979a67393903141c8db2881bb3ce7e3
ericarterbridge <ericarterbridge@gmail.com>
2022-10-19 21:41:55 -0400

    added processDesign method to designtacocontroller

--- src/main/java/sia/tacocloud/Controller/DesignTacoController.java
+++ src/main/java/sia/tacocloud/Controller/DesignTacoController.java
@@ -3,12 +3,15 @@ package sia.tacocloud.Controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import sia.tacocloud.Ingredient;
 import sia.tacocloud.Ingredient.Type;
+import sia.tacocloud.Taco;
 import sia.tacocloud.TacoCloudApplication;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -47,5 +50,15 @@ public class DesignTacoController {
         return ingredients.stream()
                 .filter(x -> x.getType().equals(type))
                 .collect(Collectors.toList());
+    }
+
+    public String processDesign(@Valid Taco design, Errors errors){
+        if (errors.hasErrors()){
+            return "design";
+        }
+
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
     }
 }
