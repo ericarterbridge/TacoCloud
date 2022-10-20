c800b92371cc5c33be100332bed5491ae33172ed
ericarterbridge <ericarterbridge@gmail.com>
2022-10-19 21:55:51 -0400

    added orderForm method to ordercontroller

--- src/main/java/sia/tacocloud/Controller/OrderController.java
+++ src/main/java/sia/tacocloud/Controller/OrderController.java
@@ -7,9 +7,16 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.slf4j.Slf4j;
+import sia.tacocloud.Order;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm(Model model){
+        model.addAttribute("order", new Order());
+        return "orderForm";
+    }
 }
