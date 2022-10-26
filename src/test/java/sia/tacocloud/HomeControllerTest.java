ec6cdc2946d342472ff09f1a55137b44ee2783f7
ericarterbridge <ericarterbridge@gmail.com>
2022-10-26 15:02:54 -0400

    deleted HomeController class

--- src/test/java/sia/tacocloud/HomeControllerTest.java
+++ src/test/java/sia/tacocloud/HomeControllerTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import sia.tacocloud.Controller.HomeController;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
