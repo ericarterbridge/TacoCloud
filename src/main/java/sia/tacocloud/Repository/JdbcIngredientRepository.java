03f834e0c5758ed730ee861e7c162e72543643c3
ericarterbridge <ericarterbridge@gmail.com>
2022-10-31 14:50:09 -0400

    added constructor for JdbcIngredientRepository

--- src/main/java/sia/tacocloud/Repository/JdbcIngredientRepository.java
+++ src/main/java/sia/tacocloud/Repository/JdbcIngredientRepository.java
@@ -1,10 +1,20 @@
 package sia.tacocloud.Repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import sia.tacocloud.Ingredient;
 
 @Repository
 public class JdbcIngredientRepository implements IngredientRepository {
+
+    private JdbcTemplate jdbc;
+
+    @Autowired
+    public JdbcIngredientRepository(JdbcTemplate jdbc){
+        this.jdbc = jdbc;
+    }
+
     @Override
     public Iterable<Ingredient> findAll() {
         return null;
