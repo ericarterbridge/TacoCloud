f2f0058ce232833d7b6c9fde083be605d6471c3a
ericarterbridge <ericarterbridge@gmail.com>
2022-10-31 14:47:18 -0400

    implemented IngredientRepository methods

--- src/main/java/sia/tacocloud/Repository/JdbcIngredientRepository.java
+++ src/main/java/sia/tacocloud/Repository/JdbcIngredientRepository.java
@@ -1,7 +1,22 @@
 package sia.tacocloud.Repository;
 
 import org.springframework.stereotype.Repository;
+import sia.tacocloud.Ingredient;
 
 @Repository
 public class JdbcIngredientRepository implements IngredientRepository {
+    @Override
+    public Iterable<Ingredient> findAll() {
+        return null;
+    }
+
+    @Override
+    public Ingredient findOne(String id) {
+        return null;
+    }
+
+    @Override
+    public Ingredient save(Ingredient ingredient) {
+        return null;
+    }
 }
