82461874820bc8f6cabfb7e9f2350bee55b6db95
ericarterbridge <ericarterbridge@gmail.com>
2022-11-02 16:26:00 -0400

    updated save method in JdbcIngredientRepository

--- src/main/java/sia/tacocloud/Repository/JdbcIngredientRepository.java
+++ src/main/java/sia/tacocloud/Repository/JdbcIngredientRepository.java
@@ -41,6 +41,11 @@ public class JdbcIngredientRepository implements IngredientRepository {
 
     @Override
     public Ingredient save(Ingredient ingredient) {
-        return null;
+        jdbc.update(
+                "insert into Ingredient (id, name, type) values (?, ?, ?)",
+                ingredient.getId(),
+                ingredient.getName(),
+                ingredient.getType().toString());
+        return ingredient;
     }
 }
