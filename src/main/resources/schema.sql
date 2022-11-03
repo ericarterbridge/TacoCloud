bb389da3e150bb125add90c0309cd486c0fb2698
ericarterbridge <ericarterbridge@gmail.com>
2022-11-03 18:36:58 -0400

    updated schema.sql file

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,3 +9,9 @@ create table if not exists Taco (
     name varchar(50) not null,
     createdAt timestamp not null
 );
+
+create table if not exists Taco_Ingredients (
+    taco bigint not null,
+    ingredient varchar(4) not null
+);
+
