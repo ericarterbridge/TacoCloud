c2b38b460cc8eaf0599bf966546cacba38cb9ea9
ericarterbridge <ericarterbridge@gmail.com>
2022-11-03 18:42:52 -0400

    updated schema.sql file

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -33,3 +33,12 @@ create table if not exists Taco_Order (
         placedAt timestamp not null
 );
 
+create table if not exists Taco_Order_Tacos (
+    tacoOrder bigint not null,
+    taco bigint not null
+);
+
+alter table Taco_Order_Tacos
+    add foreign key (tacoOrder) references Taco_Order(id);
+alter table Taco_Order_Tacos
+    add foreign key (taco) references Taco(id);
