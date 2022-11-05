d398893d8787b10723b7020643e9315f08ae808d
ericarterbridge <ericarterbridge@gmail.com>
2022-11-04 22:59:18 -0400

    updated data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,11 @@ delete from Taco_Order_Tacos;
 delete from Taco_Ingredients;
 delete from Taco;
 delete from Taco_Order;
+
+delete from Ingredient;
+insert into Ingredient (id, name, type)
+                values ('FLTO', 'Flour Tortilla', 'WRAP');
+insert into Ingredient (id, name, type)
+                values ('COTO', 'Corn Tortilla', 'WRAP');
+insert into Ingredient (id, name, type)
+                values ('GRBF', 'Ground Beef', 'PROTEIN');
