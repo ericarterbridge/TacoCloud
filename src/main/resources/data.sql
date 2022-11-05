22b39a2cb370b76f912f3a0811ac905dde46c321
ericarterbridge <ericarterbridge@gmail.com>
2022-11-04 23:00:20 -0400

    updated data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,3 +10,11 @@ insert into Ingredient (id, name, type)
                 values ('COTO', 'Corn Tortilla', 'WRAP');
 insert into Ingredient (id, name, type)
                 values ('GRBF', 'Ground Beef', 'PROTEIN');
+insert into Ingredient (id, name, type)
+                values ('CARN', 'Carnitas', 'PROTEIN');
+insert into Ingredient (id, name, type)
+                values ('TMTO', 'Diced Tomatoes', 'VEGGIES');
+insert into Ingredient (id, name, type)
+                values ('LETC', 'Lettuce', 'VEGGIES');
+insert into Ingredient (id, name, type)
+                values ('CHED', 'Cheddar', 'CHEESE');
