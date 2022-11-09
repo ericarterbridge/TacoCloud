0f216b387406867657da1e84aa99bd5e6c0b9527
ericarterbridge <ericarterbridge@gmail.com>
2022-11-08 23:58:48 -0500

    updated JdbcTacoRepository saveTacoInfo method

--- src/main/java/sia/tacocloud/Repository/JdbcTacoRepository.java
+++ src/main/java/sia/tacocloud/Repository/JdbcTacoRepository.java
@@ -1,9 +1,18 @@
 package sia.tacocloud.Repository;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import sia.tacocloud.Ingredient;
 import sia.tacocloud.Taco;
+
+import java.sql.Timestamp;
+import java.sql.Types;
+import java.util.Arrays;
+import java.util.Date;
 
 @Repository
 public class JdbcTacoRepository implements TacoRepository {
@@ -22,5 +31,22 @@ public class JdbcTacoRepository implements TacoRepository {
             saveIngredientToTaco(ingredient, tacoId);
         }
         return taco;
+    }
+
+    private long saveTacoInfo(Taco taco){
+        taco.setCreatedAt(new Date());
+        PreparedStatementCreator psc = new PreparedStatementCreatorFactory(
+                "insert into Taco (name, createdAt) values (?, ?)",
+                Types.VARCHAR, Types.TIMESTAMP
+        ).newPreparedStatementCreator(
+                Arrays.asList(
+                        taco.getName(),
+                        new Timestamp(taco.getCreatedAt().getTime())
+                )
+        );
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbc.update(psc, keyHolder);
+
+        return keyHolder.getKey().longValue();
     }
 }
