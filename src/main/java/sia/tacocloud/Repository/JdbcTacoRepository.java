dc4b66636ee9efe9b52f64668d0d7fd4bb740bfa
ericarterbridge <ericarterbridge@gmail.com>
2022-11-08 23:49:11 -0500

    updated JdbcTacoRepository

--- src/main/java/sia/tacocloud/Repository/JdbcTacoRepository.java
+++ src/main/java/sia/tacocloud/Repository/JdbcTacoRepository.java
@@ -1,4 +1,19 @@
 package sia.tacocloud.Repository;
 
-public class JdbcTacoRepository {
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import sia.tacocloud.Taco;
+
+@Repository
+public class JdbcTacoRepository implements TacoRepository {
+
+    private JdbcTemplate jdbc;
+
+    public JdbcTacoRepository(JdbcTemplate jdbc){
+        this.jdbc = jdbc;
+    }
+    @Override
+    public Taco save(Taco design) {
+        return null;
+    }
 }
