f6f9dd7ab76f0717c850576bed9caf95f8fd8265
ericarterbridge <ericarterbridge@gmail.com>
2022-11-11 19:16:01 -0500

    updated JdbcOrderRepository

--- src/main/java/sia/tacocloud/Repository/JdbcOrderRepository.java
+++ src/main/java/sia/tacocloud/Repository/JdbcOrderRepository.java
@@ -1,4 +1,13 @@
 package sia.tacocloud.Repository;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class JdbcOrderRepository {
+
+    private SimpleJdbcInsert orderInserter;
+    private SimpleJdbcInsert orderTacoInserter;
+    private ObjectMapper objectMapper;
 }
